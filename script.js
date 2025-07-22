96a876837e35265ce168dbfc567a483a27bd0e13
Joycesolis12 <joycesolis08@gmail.com>
2025-07-22 00:34:13 -0500

    scriptactualizado

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Evento onload
 function onPageLoad() {
-    document.getElementById('loadMessage').innerText = 'La página ha cargado correctamente.';
+    document.getElementById('loadMessage').innerText = 'La página ha cargado correctamente no lo olvidess.';
 }
 
 // Evento onunload
 function onPageUnload() {
-    alert('La página se está cerrando o navegando a otro lugar.');
+    alert('La página se está cerrando o navegando a otro lugar porfavor revisa.');
 }
 
 // Evento onerror y onabort
 function onObjectError() {
-    alert('Ha ocurrido un error al cargar el objeto.');
+    alert('Ha ocurrido un error al cargar el objeto porfavor revisa.');
 }
